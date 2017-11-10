3ebf2fff20ee6cdb20a1e6d8f2d7ae3eb98f5d37
Nick Ames <nickrames@gmail.com>
2017-11-10 14:48:55 -0700

    initial to do

---
+++ eight_ball.rb
@@ -0,0 +1,12 @@
+# Nick Ames
+# November 10, 2017
+
+# To Do List
+# User inputs question
+# Computer outputs random answer
+# User inputs "QUIT"
+# Computer outputs a goodbye message and exits
+
+puts "Type any question to get answer from Magic Eight Ball, or type QUIT"
+puts "The Magic Eight Ball says:"
+puts "Thanks for asking the Magic Eight Ball. Please play again!"
