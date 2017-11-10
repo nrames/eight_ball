2964b2934d7756499a20ca1ad36ee3d90fb9cd71
Nick Ames <nickrames@gmail.com>
2017-11-10 16:44:01 -0700

    basic req complete

--- eight_ball.rb
+++ eight_ball.rb
@@ -1,12 +1,30 @@
 # Nick Ames
 # November 10, 2017
+require 'colorize'
 
-# To Do List
-# User inputs question
-# Computer outputs random answer
-# User inputs "QUIT"
-# Computer outputs a goodbye message and exits
+@response = ["Follow your heart", "You do you", "No way", "Maybe", "Ask your mother", "Not sure yet", "ask again", "Just do it", "Concentrate and ask again" ]
 
-puts "Type any question to get answer from Magic Eight Ball, or type QUIT"
-puts "The Magic Eight Ball says:"
-puts "Thanks for asking the Magic Eight Ball. Please play again!"
+def question
+  puts "Type any question to get answer from Magic Eight Ball, or type QUIT""\n".yellow
+  gets.strip.to_s
+  answer
+end  
+    
+def answer
+  puts "The Magic Eight Ball says:".yellow 
+  puts @response.sample.red
+  puts "Type QUIT to exit, or ask another question.".green  
+  choice
+end
+
+def choice
+  response = gets.strip.to_s.downcase
+  if response == "quit"
+    puts "Thanks for asking the Magic Eight Ball. Goodbye!".yellow
+    exit
+  else
+    answer
+  end
+end
+
+question
